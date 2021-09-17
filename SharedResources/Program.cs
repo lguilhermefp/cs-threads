084b7cc765f11c19a6144f0670ea20ab71bae7c7
luis guilherme <luis-guilherme.pereira@unesp.br>
2021-09-17 19:57:53 -0300

    shareed resources

---
+++ SharedResources/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace SharedResources
+{
+    class Program
+    {
+		private static bool isCompleted;
+		static readonly object lockCompleted = new object();
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(HelloWorld);
+			thread.Start();
+
+			HelloWorld();
+        }
+
+		private static void HelloWorld()
+		{
+			lock(lockCompleted)
+			{
+				if(!isCompleted)
+				{
+					Console.WriteLine("Hello world should be printed just once");
+					isCompleted = true;
+				}
+			}
+		}
+    }
+}
