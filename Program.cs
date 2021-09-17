342faa1740a9d05049146ce628eb36133f4c68bf
luis guilherme <luis-guilherme.pereira@unesp.br>
2021-09-17 19:34:37 -0300

    thread name

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(WriteUsingNewThread);
+			thread.Name = "Cazton Worker";
+			thread.Start();
+			Thread.CurrentThread.Name = "Cazton Main";
+			for(int i=0; i<1000; i++)
+			{
+				Console.Write(" A"+i+" ");
+			}
+        }
+
+		private static void WriteUsingNewThread()
+		{
+			for(int i=0; i<1000; i++)
+			{
+				Console.Write(" Z"+i+" ");
+			}
+		}
+    }
+}
