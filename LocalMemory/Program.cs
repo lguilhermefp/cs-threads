41429892857d077a71287de66a4604db41210f38
luis guilherme <luis-guilherme.pereira@unesp.br>
2021-09-17 20:04:53 -0300

    local memory

---
+++ LocalMemory/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace LocalMemory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Thread(PrintOneToThirty).Start();
+
+			PrintOneToThirty();
+        }
+
+		private static void PrintOneToThirty()
+		{
+			for(int i=0; i<30; i++)
+			{
+				Console.Write(i + 1 + " ");
+			}
+		}
+    }
+}
